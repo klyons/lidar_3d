bff681ca25f0a3bbb87b1fce3ab583cf9a337ba7
klyons <lyons.kelly@gmail.com>
2026-05-29 17:42:26 -0700

    adding the rest of our code
    Signed-off-by: klyons <lyons.kelly@gmail.com>

---
+++ test.ts
@@ -0,0 +1,31 @@
+let myName: string = "Kelly";
+let city: string = "New York";
+let age: number = 30;
+let shoeSize: number = 10;
+
+
+
+console.log(myName);
+console.log(city);
+console.log(age);
+console.log(shoeSize);
+
+
+
+
+let programmingLanguages: string[] = ["TypeScript", "JavaScript", "Python", "Java"];
+
+let student: [string, number] = ["Alice", 25];
+
+enum Color {
+	Red,
+	Green,
+	Blue
+}
+
+const favoriteColor: Color = Color.Green;
+
+console.log(programmingLanguages);
+console.log(student);
+console.log(favoriteColor);
+
